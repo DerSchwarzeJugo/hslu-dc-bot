fe1f6a9c51c7b9d36f1f5b27fb8fc11bd1f39327
Luka Stojic <luka@arthaus.info>
2020-10-29 00:52:49 +0100

    added own help function

--- hslu-dc-bot.py
+++ hslu-dc-bot.py
@@ -13,7 +13,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 BOTCHANNEL_ID = os.getenv('BOTCHANNEL_ID')
 ADMINGROUP_ID = os.getenv('ADMINGROUP_ID')
 
-bot = commands.Bot(command_prefix='$')
+# set up the bot object
+bot = commands.Bot(command_prefix='$', help_command=None)
 
 # if command not in botchannel
 async def channelCheck(ctx, channel):
@@ -56,6 +57,50 @@ async def innerChannelCheck(ctx, channel):
         return False
     return True
 
+# botchannel help
+async def setUpHelpEmbed(ctx):
+    cp = bot.command_prefix
+    embed = discord.Embed(
+        colour=discord.Colour.gold(),
+        title="BotHelp",
+        description=f"{cp}help Zeigt dieses Menü"
+    )
+    adEmbed = discord.Embed(
+        colour=discord.Colour.red(),
+        title="BotHelp",
+        description=f"Admins functions"
+    )
+    # botchannel help
+    if ctx.channel.id == int(BOTCHANNEL_ID):
+        help = f'[{cp}np | {cp}nP] \nErzeugt ein neues Projekt mit einem Textchat\n--> {cp}newProject NAME || {cp}np "name mit abständen"'
+        embed.add_field(name=f"{cp}newProject", value=help, inline=False)
+        help = f"[{cp}nu | {cp}nU] \nFügt einen oder mehrere User einem Projekt hinzu\n--> {cp}newUser PROJECT @USER [@USER...]"
+        embed.add_field(name=f"{cp}newUser", value=help, inline=False)
+        await ctx.channel.send(embed=embed)
+    
+        role = ctx.guild.get_role(int(ADMINGROUP_ID))
+        if role in ctx.author.roles:
+            help = f'[{cp}ap | {cp}aP] \nNeue Kategorie mit multiplen Text & Voice channels\n--> {cp}ap NAME T V'
+            adEmbed.add_field(name=f"{cp}adminProject", value=help, inline=False)
+            help = f"[{cp}dp | {cp}dP] \nProjekt inkl. aller channels löschen\n--> {cp}dP NAME"
+            adEmbed.add_field(name=f"{cp}deleteProject", value=help, inline=False)
+            await ctx.channel.send(embed=adEmbed)
+
+# botbefehle help
+async def setUpInlineHelpEmbed(channel):
+    cp = bot.command_prefix
+    embed = discord.Embed(
+        colour=discord.Colour.gold(),
+        title="BotHelp",
+        description=f"{cp}help Zeigt dieses Menü"
+    )
+    if channel.name == "botbefehle":
+        help = f'[{cp}nt | {cp}nT] \nErzeugt einen neuen Textchat in diesem Projekt\n--> {cp}newText | {cp}newText NAME'
+        embed.add_field(name=f"{cp}newText", value=help, inline=False)
+        help = f'[{cp}nV | {cp}nV] \nErzeugt einen neuen Voicechat in diesem Projekt\n--> {cp}nv | {cp}nv NAME'
+        embed.add_field(name=f"{cp}newVoice", value=help, inline=False)
+        await channel.send(embed=embed)
+
 # dockerstyle names for random chats
 def getFancyName():
     name: str
@@ -73,7 +118,13 @@ async def on_ready():
         f.write(str(datetime.now()) + f" {bot.user.name} - {bot.user.id} login\n")
 
 
-@bot.command(name='newProject', help='Erzeugt ein neues Projekt mit Textchat', aliases=["nP", "np"])
+@bot.command(name="help", aliases=["HELP","helpp","heelp"])
+async def help(ctx):
+    await setUpHelpEmbed(ctx)
+    await setUpInlineHelpEmbed(ctx.channel)
+
+
+@bot.command(name='newProject', aliases=["nP", "np"])
 async def newProject(ctx, projectName=""):
     channel = bot.get_channel(ctx.channel.id)
     if not await channelCheck(ctx, channel) or not await projectNameCheck(projectName, channel) or not await categoryCheck(ctx, projectName):
@@ -87,13 +138,14 @@ async def newProject(ctx, projectName=""):
         ctx.author: discord.PermissionOverwrite(read_messages=True)
     }
     category = await ctx.guild.create_category(projectName, overwrites = overwrites)
-    await ctx.guild.create_text_channel("botbefehle", category=category)
+    botchannel= await ctx.guild.create_text_channel("botbefehle", category=category)
     await ctx.guild.create_text_channel("chat 1", category=category)
+    await setUpInlineHelpEmbed(botchannel)
     with open("botLog.log", "a+") as f:
         f.write(str(datetime.now()) + f" {ctx.author} - {ctx.command} cmd\n")
 
 
-@bot.command(name='adminProject', help='Erzeugt eine neue Kategorie mit bestimmter anzahl channels (admins only)', aliases=["aP", "ap"])
+@bot.command(name='adminProject', aliases=["aP", "ap"])
 async def adminProject(ctx, projectName="", textChannels=1, voiceChannels=1):
     channel = bot.get_channel(ctx.channel.id)
     if not await adminCheck(ctx, channel) or not await channelCheck(ctx, channel) or not await projectNameCheck(projectName, channel) or not await categoryCheck(ctx, projectName):
@@ -107,7 +159,8 @@ async def adminProject(ctx, projectName="", textChannels=1, voiceChannels=1):
         ctx.author: discord.PermissionOverwrite(read_messages=True)
     }
     category = await ctx.guild.create_category(projectName, overwrites=overwrites)
-    await ctx.guild.create_text_channel("botbefehle", category=category)
+    botchannel = await ctx.guild.create_text_channel("botbefehle", category=category)
+    await setUpInlineHelpEmbed(botchannel)
     for i in range(textChannels):
         count = i + 1
         await ctx.guild.create_text_channel(f"chat {count}", category=category)
@@ -118,7 +171,7 @@ async def adminProject(ctx, projectName="", textChannels=1, voiceChannels=1):
         f.write(str(datetime.now()) + f" {ctx.author} - {ctx.command} cmd\n")
 
 
-@bot.command(name="deleteProject", help="Delete a project and all channels in it (admin only, careful!)", aliases=["dP", "dp"])
+@bot.command(name="deleteProject", aliases=["dP", "dp"])
 async def deleteProject(ctx, projectName=""):
     channel = bot.get_channel(ctx.channel.id)
     
@@ -152,7 +205,7 @@ async def deleteProject(ctx, projectName=""):
             f.write(str(datetime.now()) + f" {ctx.author} - {ctx.command} - {projectName} cmd\n")
 
 
-@bot.command(name='newText', help='Erzeugt einen neuen Textchat', aliases=["nT", "nt"])
+@bot.command(name='newText', aliases=["nT", "nt"])
 async def newText(ctx, tcName=""):
     channel = bot.get_channel(ctx.channel.id)
     if not await innerChannelCheck(ctx, channel):
@@ -162,9 +215,11 @@ async def newText(ctx, tcName=""):
         tcName = getFancyName()
     await channel.send(f'Neuer Textchannel **{tcName}** wird erstellt 🦾')
     await ctx.guild.create_text_channel(f"{tcName}", category=channel.category)
+    with open("botLog.log", "a+") as f:
+        f.write(str(datetime.now()) + f" {ctx.author} - {ctx.command} cmd\n")
 
 
-@bot.command(name='newVoice', help='Erzeugt einen neuen Voicechat', aliases=["nV", "nv"])
+@bot.command(name='newVoice', aliases=["nV", "nv"])
 async def newVoice(ctx, vcName=""):
     channel = bot.get_channel(ctx.channel.id)
     if not await innerChannelCheck(ctx, channel):
@@ -174,9 +229,11 @@ async def newVoice(ctx, vcName=""):
         vcName = getFancyName()
     await channel.send(f'Neuer Voicechannel **{vcName}** wird erstellt 🦾')
     await ctx.guild.create_voice_channel(f"{vcName}", category=channel.category)
+    with open("botLog.log", "a+") as f:
+        f.write(str(datetime.now()) + f" {ctx.author} - {ctx.command} cmd\n")
         
 
-@bot.command(name="newUser", help="Fügt einen oder mehrere User einem Projekt hinzu", aliases=["nU", "nu"])
+@bot.command(name="newUser", aliases=["nU", "nu"])
 async def newUser(ctx, projectName=""):
     channel = bot.get_channel(ctx.channel.id)
     if not await channelCheck(ctx, channel) or not await projectNameCheck(projectName, channel):
@@ -190,10 +247,12 @@ async def newUser(ctx, projectName=""):
     for mention in ctx.message.mentions:
         user = bot.get_user(mention.id)
         if not isinstance(user, discord.abc.User):
-            await channel.send("Kein valider user. markiere mit @ und wähle die Person(en) aus der Liste 🤖")
+            await channel.send("Kein valider user. Markiere mit @ und wähle die Person(en) aus der Liste 🤖")
             return
         await category.set_permissions(user, read_messages=True)
-        await channel.send(f"User {user} wurde dem Projekt {projectName} hinzugefügt 🤖")
+        await channel.send(f"User {user} wurde dem **Projekt** {projectName} hinzugefügt 🤖")
+        with open("botLog.log", "a+") as f:
+            f.write(str(datetime.now()) + f" {ctx.author} - {ctx.command} - {user} - {projectName} cmd\n")
         return
     # only happening if ctx.message.mentions is empty
     await channel.send("Keine validen User angegeben 🤖")
